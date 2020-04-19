ca919aa6b3ad76eab3345c874114d918d34e751a
AnnRoy27 <annroy2708@gmail.com>
2020-04-19 17:19:25 +0530

    Test

--- JenknPro/src/main/java/pack/Sample.java
+++ JenknPro/src/main/java/pack/Sample.java
@@ -2,12 +2,16 @@ package pack;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Sample {
 
-	public static void main(String[] args) {
+@Test
+public void test1()
+{
+
 		// TODO Auto-generated method stub
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
